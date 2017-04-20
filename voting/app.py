362b8a4ffa5bc9ffdfb7d314d8953654aa261d56
Torrey Jones <torreypjones@gmail.com>
2017-04-20 13:52:30 -0600

    chagned to black/white

--- voting/app.py
+++ voting/app.py
@@ -9,8 +9,8 @@ import logging
 import signal
 import sys
 
-optionA = "Futbol"
-optionB = "soccer"
+optionA = "black"
+optionB = "white"
 name = optionA + " VS " + optionB
 
 redis = Redis(host="redis", db=0)
